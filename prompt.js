2facc44d60dfcc9ae26332069a44de8b7cd84331
xcibe95x <xcibe95x@gmail.com>
2022-05-23 15:43:07 +0200

    Simple JS Prompt Form

--- prompt.js
+++ prompt.js
@@ -1,1 +1,11 @@
 
+
+window.onload = function() {
+let xname = prompt("Insert your Name");
+let surname = prompt("Insert your Surname");
+let birthday = prompt("Insert your birthday");
+let birthplace = prompt("Insert your birth place");
+let zodiacSign = prompt("Insert your zodiac sign");
+
+document.getElementById("data").innerHTML= `<p><b>Name:</b> ${xname}</p> <p><b>Surname:</b> ${surname}</p> <p><b>Birthday:</b> ${birthday}</p>  <p><b>Birthplace:</b> ${birthplace}</p> <p><b>Zodiac Sign:</b> ${zodiacSign}</p>`;;
+}
